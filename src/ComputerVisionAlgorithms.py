b09dc9e45b2536f294fdc75f7f29cb9306d6eebd
Daniel Hoven <dhoven@my.gcu.edu>
2021-01-08 12:46:20 -0700

    add gitignore

---
+++ src/ComputerVisionAlgorithms.py
@@ -0,0 +1,5 @@
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+
+def test():
+    print("Computer can see you now\n")
+    return
---
+++ src/main.py
@@ -0,0 +1,28 @@
+###########################################################################################################################
+#
+#    Main method for Autonomous Quadcopter
+#
+#    Author: Daniel Hoven
+#
+#    Date Opened: 1/8/21
+#
+#    Remote: https://github.com/Dhoven23/CapstoneProject
+#
+#--------------------------------------------------------------------------------------------------------------------------
+
+import cv2
+
+import numpy as np
+
+import serial
+
+import src.ComputerVisionAlgorithms as CVA
+
+def main():
+    print("Entry Point Formed!")
+
+    CVA.test()
+
+if __name__ == '__main__':
+    main()
+
