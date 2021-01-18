fa5018b6e182a43c662619ea1297919261a809db
Daniel Hoven <dhoven@my.gcu.edu>
2021-01-18 10:04:17 -0700

    add trapezoidal integrator

--- src/main.py
+++ src/main.py
@@ -11,15 +11,32 @@
 #--------------------------------------------------------------------------------------------------------------------------
 
 import cv2
-
 import numpy as np
-
 import serial
+import ComputerVisionAlgorithms as CVA
+import time
+#/////////////
+# Initialize sensors, motors, and telemetry
+#/////////////
 
-import src.ComputerVisionAlgorithms as CVA
+def TeensyConnect():
+    teensy = serial.Serial('/dev/cu.usbmodem81837901')
+    if teensy.isOpen():
+        teensy.close()
+    teensy.open()
+    time.sleep(0.5)
+    for i in range (0,5): teensy.write(bytes(b'N'))
+    time.sleep(0.5)
+    if (teensy.inWaiting() > 0):
+        reply = teensy.readline()
+        reply = str(reply)
+        print(reply)
+
 
 def main():
+
     print("Entry Point Formed!")
+    TeensyConnect()
 
     CVA.test()
 
